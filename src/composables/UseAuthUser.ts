1041981c147288ad24a36e5518786f1e7a84a452
Fausto André <fslawliet@gmail.com>
2023-03-11 10:18:59 -0300

    feat: composable de usuários no supabase

---
+++ src/composables/UseAuthUser.ts
@@ -0,0 +1,81 @@
+import { ref } from 'vue';
+import useSupabase from 'boot/supabase';
+import { Provider } from '@supabase/supabase-js';
+
+const user = ref(null);
+
+export const useAuthUser = () => {
+  const { supabase } = useSupabase();
+
+  const login = async ({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) throw error;
+    return data;
+  };
+  const loginWithSocialProvider = async (provider: Provider) => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider,
+    });
+    if (error) throw error;
+    return data;
+  };
+  const logout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) throw error;
+  };
+  const isLoggedInIn = async () => {
+    return !!user.value;
+  };
+
+  const register = async ({
+    email,
+    password,
+    ...meta
+  }: {
+    email: string;
+    password: string;
+  }) => {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: meta,
+        emailRedirectTo: `${window.location.origin}/me?fromEmail=registrationConfirmation`,
+      },
+    });
+    if (error) throw error;
+    return data;
+  };
+  const update = async (userData: { email?: string; password?: string }) => {
+    const { data, error } = await supabase.auth.updateUser(userData);
+    if (error) throw error;
+    return data;
+  };
+  const sendPasswordResetEmail = async (email: string) => {
+    const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+      redirectTo: 'https://example.com/update-password',
+    });
+    if (error) throw error;
+    return data;
+  };
+
+  return {
+    user,
+    login,
+    loginWithSocialProvider,
+    logout,
+    isLoggedInIn,
+    register,
+    update,
+    sendPasswordResetEmail,
+  };
+};
