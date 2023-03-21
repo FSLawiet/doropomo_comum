b1e6c1e93db6bd87d8e5983b1466f462fac49b1c
Fausto André <fslawliet@gmail.com>
2023-03-21 14:03:51 -0300

    Testes Service Worker

--- src-pwa/register-service-worker.ts
+++ src-pwa/register-service-worker.ts
@@ -13,19 +13,23 @@ register(process.env.SERVICE_WORKER_FILE, {
   // registrationOptions: { scope: './' },
 
   ready(/* registration */) {
-    // console.log('Service worker is active.')
+    console.log('Service worker is active.');
   },
 
   registered(/* registration */) {
-    // console.log('Service worker has been registered.')
+    console.log('Service worker has been registered.');
   },
 
   cached(/* registration */) {
-    // console.log('Content has been cached for offline use.')
+    console.log('Content has been cached for offline use.');
   },
 
   updatefound(/* registration */) {
-    // console.log('New content is downloading.')
+    console.log('New content is downloading.');
+    Notify.create({
+      type: 'ongoing',
+      message: 'Baixando novo conteúdo...',
+    });
   },
 
   updated(registration) {
@@ -43,10 +47,12 @@ register(process.env.SERVICE_WORKER_FILE, {
   },
 
   offline() {
-    // console.log('No internet connection found. App is running in offline mode.')
+    console.log(
+      'No internet connection found. App is running in offline mode.'
+    );
   },
 
-  error(/* err */) {
-    // console.error('Error during service worker registration:', err)
+  error(err) {
+    console.error('Error during service worker registration:', err);
   },
 });
