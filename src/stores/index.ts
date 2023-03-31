78068459de2a0111963785b1d4deedc1d280cc6a
Fausto André <fslawliet@gmail.com>
2023-03-31 00:36:18 -0300

    docker

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,5 @@
-import { store } from 'quasar/wrappers'
-import { createPinia } from 'pinia'
+import { store } from 'quasar/wrappers';
+import { createPinia } from 'pinia';
 import { Router } from 'vue-router';
 
 /*
@@ -22,11 +22,12 @@ declare module 'pinia' {
  * with the Store instance.
  */
 
-export default store((/* { ssrContext } */) => {
-  const pinia = createPinia()
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default store((/*{ ssrContext }*/) => {
+  const pinia = createPinia();
 
   // You can add Pinia plugins here
   // pinia.use(SomePiniaPlugin)
 
-  return pinia
-})
+  return pinia;
+});
