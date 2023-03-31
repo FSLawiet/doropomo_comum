8911f14807a3dde474147421874be660d3e1b8e7
Fausto André <fslawliet@hotmail.com>
2023-03-31 16:15:02 -0300

    Docker

--- quasar.config.js
+++ quasar.config.js
@@ -138,7 +138,7 @@ module.exports = configure(function (ctx) {
     // sourceFiles: {
     //   rootComponent: 'src/App.vue',
     //   router: 'src/router/index',
-    //   store: 'src/store/index',
+    //   store: 'src/stores/index',
     //   registerServiceWorker: 'src-pwa/register-service-worker',
     //   serviceWorker: 'src-pwa/custom-service-worker',
     //   pwaManifestFile: 'src-pwa/manifest.json',
